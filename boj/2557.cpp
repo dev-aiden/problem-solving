82427cbf6c6ab74d4a79dac73570d3603a6ffb93
Gwangyong Jeong <tyjk32@gmail.com>
2021-08-16 02:24:38 +0900

    Update 2557.cpp

--- boj/2557.cpp
+++ boj/2557.cpp
@@ -3,9 +3,7 @@
 using namespace std;
 
 int main(void) {
-	ios_base::sync_with_stdio(false);
-    
-	cout << "Hello World!" << '\n';
-    
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cout << "Hello World!" << '\n';
+    return 0;
 }
