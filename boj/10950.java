ae9a87e0fc604a878528e4dc25c2848d14fd57f0
Gwangyong Jeong <tyjk32@gmail.com>
2021-08-22 11:41:02 +0900

    Update 10950.java

--- boj/10950.java
+++ boj/10950.java
@@ -1,13 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
         for(int i = 0; i < t; ++i) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            System.out.println(a + b);
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append('\n');
         }
+        System.out.println(sb);
+        br.close();
     }
 }
