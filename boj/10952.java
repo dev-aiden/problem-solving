fdc144093e3a9fafabdeaadb8c8152610570d8d0
Gwangyong Jeong <tyjk32@gmail.com>
2021-08-22 11:48:20 +0900

    Update 10952.java

--- boj/10952.java
+++ boj/10952.java
@@ -1,13 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         while(true) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
+            String str = br.readLine();
+            int a = str.charAt(0) - 48;
+            int b = str.charAt(2) - 48;
             if(a == 0 && b == 0) break;
-            System.out.println(a + b);
+            sb.append(a + b).append('\n');
         }
+        System.out.println(sb);
+        br.close();
     }
 }
