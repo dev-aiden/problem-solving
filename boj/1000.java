dcebe537274d1acd75774e171f9a5ae42d142ff1
Gwangyong Jeong <tyjk32@gmail.com>
2021-08-22 11:31:35 +0900

    Update 1000.java

--- boj/1000.java
+++ boj/1000.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(a + b);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str.charAt(0) + str.charAt(2) - 96);
+        br.close();
     }
 }
