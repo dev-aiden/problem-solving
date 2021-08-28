cd9307d41a2364726fa5e95cf18ca31172a787ce
Gwangyong Jeong <tyjk32@gmail.com>
2021-08-29 03:08:54 +0900

    Update bubbleSort.java

--- algorithm/bubbleSort.java
+++ algorithm/bubbleSort.java
@@ -44,7 +44,7 @@ public class BubbleSort {
             }
         }
 
-        System.out.print("오름차순 정렬 후 : ");
+        System.out.print("내림차순 정렬 후 : ");
         for(int i = 0; i < 5; ++i) System.out.print(arr2[i] + " ");
         System.out.println();
     }
