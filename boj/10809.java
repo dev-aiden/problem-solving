531d99edac8c89d2320a0bcb867007c541a31e46
Gwangyong Jeong <tyjk32@gmail.com>
2021-09-23 19:39:08 +0900

    Create 10809.java

---
+++ boj/10809.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int arr[] = new int[26];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 26; ++i) arr[i] = -1;
+        String s = br.readLine();
+        int len = s.length();
+        for(int i = 0; i < len; ++i) {
+            if(arr[s.charAt(i) - 97] == -1) arr[s.charAt(i) - 97] = i;
+        }
+        for(int i = 0; i < 26; ++i) sb.append(arr[i]).append(" ");
+        sb.append("\n");
+        System.out.println(sb);
+    }
+}
