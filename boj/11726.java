d9bec9044d0f87f7db982e05f0204161c952591e
Gwangyong Jeong <tyjk32@gmail.com>
2021-09-30 20:58:37 +0900

    Create 11726.java

---
+++ boj/11726.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int[] d = new int[1003];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(solve(n));
+    }
+
+    public static int solve(int num) {
+        if (num < 2) return 1;
+        if (d[num] != 0) return d[num];
+        return d[num] = (solve(num - 2) + solve(num - 1)) % 10007;
+    }
+}
