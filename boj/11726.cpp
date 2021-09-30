1ff3874f7f5e7f30d7056a2ad1f01b5674ab446a
Gwangyong Jeong <tyjk32@gmail.com>
2021-09-30 20:57:34 +0900

    Create 11726.cpp

---
+++ boj/11726.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int d[1003];
+
+int solve(int num) {
+    if (num < 2) return 1;
+    if (d[num] != 0) return d[num];
+    return d[num] = (solve(num - 2) + solve(num - 1)) % 10007;
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    int n; cin >> n;
+    cout << solve(n) << "\n";
+    return 0;
+}
