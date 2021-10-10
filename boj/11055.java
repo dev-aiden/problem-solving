77be5b17f2d288b883eda420edae8556b9013792
Gwangyong Jeong <tyjk32@gmail.com>
2021-10-10 13:33:00 +0900

    Create 11055.java

---
+++ boj/11055.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int d[] = new int[1003];
+    static int a[] = new int[1003];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int ans = 0;
+        for (int i = 1; i <= n; ++i) {
+            a[i] = Integer.parseInt(st.nextToken());
+            solve(i);
+        }
+        d[1] = a[1];
+        for (int i = 1; i <= n; ++i) if(ans < d[i]) ans = d[i];
+        System.out.println(ans);
+    }
+
+    public static int solve(int n) {
+        if (d[n] > 0) return d[n];
+        d[n] = a[n];
+        for (int i = n - 1; i >= 1; --i) {
+            if (a[n] > a[i] && d[n] < solve(i) + a[n]) d[n] = d[i] + a[n];
+        }
+        return d[n];
+    }
+}
