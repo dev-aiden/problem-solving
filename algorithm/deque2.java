57c7b454404172a02a428d381d6130f5a46cbd75
Gwangyong Jeong <tyjk32@gmail.com>
2021-10-10 14:05:27 +0900

    Update deque2.java

--- algorithm/deque2.java
+++ algorithm/deque2.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class DequeExample {
 
     static Deque<Integer> d = new ArrayDeque<>();
 
