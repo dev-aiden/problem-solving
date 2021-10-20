2717458304999711fc709b1ab5086992e97e9688
Gwangyong Jeong <tyjk32@gmail.com>
2021-10-20 14:28:51 +0900

    Update 10430.cpp

--- boj/10430.cpp
+++ boj/10430.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int main(void) {
-	ios_base::sync_with_stdio(false);
-	int a, b, c; cin >> a >> b >> c;
-	cout << (a + b) % c << "\n";
-	cout << ((a % c) + (b % c)) % c << "\n";
-	cout << (a * b) % c << "\n";
-	cout << ((a % c) * (b % c)) % c << "\n";
-	return 0;
+    ios_base::sync_with_stdio(false);
+    int a, b, c; cin >> a >> b >> c;
+    cout << (a + b) % c << "\n";
+    cout << ((a % c) + (b % c)) % c << "\n";
+    cout << (a * b) % c << "\n";
+    cout << ((a % c) * (b % c)) % c << "\n";
+    return 0;
 }
