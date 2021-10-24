1d99fd60e4cfead719b09dae71acddff93e72438
Gwangyong Jeong <tyjk32@gmail.com>
2021-10-24 18:42:33 +0900

    Create euclidean.cpp

---
+++ algorithm/euclidean.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return a * b / gcd(a, b);
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    int a, b; cin >> a >> b;
+    cout << gcd(a, b) << "\n";
+    cout << lcm(a, b) << "\n";
+    return 0;
+}
