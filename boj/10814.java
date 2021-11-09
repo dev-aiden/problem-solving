010b8e44afbf2557ad527214111889abd8b8027d
Gwangyong Jeong <tyjk32@gmail.com>
2021-11-09 17:12:20 +0900

    Create 10814.java

---
+++ boj/10814.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[][] arr = new String[n][2];
+        for(int i = 0; i < n; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+        Arrays.sort(arr, (o1, o2) -> Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]));
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; ++i) sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        System.out.println(sb);
+    }
+}
