ddcbce4145ab4c6d786e8d95b5a18eddc420b523
Gwangyong Jeong <tyjk32@gmail.com>
2021-11-17 17:34:44 +0900

    Update 11004.cpp

--- boj/11004.cpp
+++ boj/11004.cpp
@@ -9,10 +9,7 @@ int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     int n, k; cin >> n >> k;
-    for (int i = 0; i < n; ++i) {
-        int num; cin >> num;
-        arr[i] = num;
-    }
+    for (int i = 0; i < n; ++i) cin >> arr[i];
     sort(arr, arr + n);
     cout << arr[k - 1] << "\n";
     return 0;
