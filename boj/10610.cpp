e7f63b2a7812c0078c7df773f591c18ff9675572
dev-aiden <tyjk32@gmail.com>
2022-05-20 20:24:54 +0900

    Update 10610

--- boj/10610.cpp
+++ boj/10610.cpp
@@ -1,39 +1,27 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-vector<int> vec[20003];
-int check[20003];
-bool ans;
-
-void dfs(int num, int val) {
-    check[num] = val;
-    int len = vec[num].size();
-    for(int i = 0; i < len; ++i) {
-        if(check[vec[num][i]] == 0) dfs(vec[num][i], 3 - val);
-        else if(check[vec[num][i]] == val) ans = false;
-    }
-}
+int arr[13];
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    int k; for(cin >> k; k--;) {
-        int v, e; cin >> v >> e;
-        for(int i = 1; i <= v; ++i) {
-            vec[i].clear();
-            check[i] = 0;
-        }
-        ans = true;
-        for(int i = 0; i < e; ++i) {
-            int a, b; cin >> a >> b;
-            vec[a].push_back(b); vec[b].push_back(a);
-        }
-        for(int i = 1; i <= v; ++i) {
-            if(check[i] == 0) dfs(i, 1);
-        }
-        cout << (ans ? "YES" : "NO") << "\n";
+    string s; cin >> s;
+    int len = s.length(), sum = 0;
+    for(int i = 0; i < len; ++i) {
+        sum += s[i] - '0';
+        ++arr[s[i] - '0'];
     }
+    if(arr[0] == 0 || (sum % 3 != 0)) {
+        cout << -1;
+    } else {
+        for(int i = 9; i >= 0; --i) {
+            for(int j = 0; j < arr[i]; ++j) {
+                cout << i;
+            }
+        }
+    }
+    cout << "\n";
     return 0;
 }
