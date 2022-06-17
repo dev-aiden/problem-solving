7f807df9119ad305aac449493834baf54fdc0c7b
dev-aiden <tyjk32@gmail.com>
2022-06-17 21:33:46 +0900

    Update 10816

--- boj/10816.java
+++ boj/10816.java
@@ -17,36 +17,14 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; ++i) {
             int num = Integer.parseInt(st.nextToken());
-            num += 10000000;
-            if(cnt[num] == 0) {
-                arr[idx++] = num;
-                ++cnt[num];
-            } else {
-                ++cnt[num];
-            }
+            ++cnt[num + 10000000];
         }
-        Arrays.sort(arr, 0, idx);
         m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for(int i = 0; i < m; ++i) arr2[i] = Integer.parseInt(st.nextToken());
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < m; ++i) sb.append(bSearch(arr2[i] + 10000000)).append(" ");
+        for(int i = 0; i < m; ++i) sb.append(cnt[arr2[i] + 10000000]).append(" ");
         sb.append("\n");
         System.out.println(sb);
-    }
-
-    public static int bSearch(int num) {
-        int start = 0, end = idx - 1;
-        while(start <= end) {
-            int mid = (start + end) / 2;
-            if(arr[mid] == num) {
-                return cnt[num];
-            } else if(arr[mid] > num) {
-                end = mid - 1;
-            } else if(arr[mid] < num) {
-                start = mid + 1;
-            }
-        }
-        return 0;
     }
 }
